97e98cc2fefdf35c37c7bf7eb472fc70567f65c5
Yonatan Kinfe <158090444+Yonatankinfe@users.noreply.github.com>
2024-09-13 11:57:58 +0300

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "unity_university_project"
+include(":app")
+ 
